c2f08612105e46673a87ef1547dc652ff94241e9
Sam Soffes <sam@soff.es>
2019-02-04 00:01:18 -0800

    Add thanks plugin

---
+++ _plugins/thanks.rb
@@ -0,0 +1,20 @@
+class Thanks < Jekyll::Generator
+  safe true
+
+  def generate(site)
+    @site = site
+    site.posts.docs.each do |document|
+      next unless thanks = document.data['thanks']
+      document.data['thanks'] = html_for(thanks)
+    end
+
+    puts '        - Thanks'
+  end
+
+  private
+
+  def html_for(markdown)
+    @_markdown ||= @site.find_converter_instance(Jekyll::Converters::Markdown)
+    @_markdown.convert(markdown)
+  end
+end
