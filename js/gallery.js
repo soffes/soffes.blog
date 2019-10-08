d289b9698cbd75925671e214687d520a039ae1f8
Sam Soffes <sam@soff.es>
2019-10-08 06:29:58 -0700

    Add arrow keys

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,9 @@ addLoadEvent(function() {
     return;
   }
 
+  // Find all gallery anchors
+  const anchors = document.querySelectorAll('div.gallery a');
+
   // Close lightbox
   const close = function() {
     if (!window.lightbox) {
@@ -36,6 +39,22 @@ addLoadEvent(function() {
       if (event.keyCode === 27) {
         close();
         event.preventDefault();
+        return;
+      }
+
+
+      // Previous
+      if (event.keyCode === 37) {
+        showPrevious();
+        event.preventDefault();
+        return;
+      }
+
+      // Next
+      if (event.keyCode === 39) {
+        showNext();
+        event.preventDefault();
+        return;
       }
     });
 
@@ -44,15 +63,62 @@ addLoadEvent(function() {
 
   // Show an image. This expects an `<a>` as its parameter.
   const show = function(element) {
+    if (!element) {
+      return;
+    }
+
     setup();
+
+    // Reset
+    window.lightbox.innerHTML = '';
 
     const image = element.children[0].cloneNode(true);
     image.removeAttribute('style');
     window.lightbox.appendChild(image);
   }
 
+  // Find the index of the current lightbox image
+  const currentIndex = function() {
+    if (!window.lightbox || !window.lightbox.children[0]) {
+      return null;
+    }
+
+    const src = window.lightbox.children[0].getAttribute('src');
+    let index = 0;
+
+    for (let anchor of anchors) {
+      if (anchor.children[0].getAttribute('src') === src) {
+        return index;
+      }
+
+      index += 1;
+    }
+
+    return null;
+  }
+
+  // Show previous image
+  const showPrevious = function() {
+    const index = currentIndex();
+    if (index === null || index - 1 === -1) {
+      return;
+    }
+
+    show(anchors[index - 1]);
+  };
+
+  // Show next image
+  const showNext = function() {
+    const index = currentIndex();
+    if (index === null || index + 1 === anchors.length) {
+      return;
+    }
+
+    show(anchors[index + 1]);
+  };
+
   // Find all gallery links and make them show the lightbox
-  for (let anchor of document.querySelectorAll('div.gallery a')) {
+  for (let anchor of anchors) {
     anchor.addEventListener('click', function(event) {
       event.preventDefault();
       show(anchor);
