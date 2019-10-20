b6a2205cd25e277ba466a53dfca3ecd3bc5a27d7
Sam Soffes <sam@soff.es>
2019-10-20 16:43:16 -0700

    Post process images
    This avoids treating the post's markdown as HTML in the process images
    step. I didn't realize this broke block quotes. This also puts it in a
    more logical spot. Rewriting the paths and thumbnailing should be
    separate.

---
+++ _plugins/process_image.rb
@@ -0,0 +1,83 @@
+require 'nokogiri'
+require 'mini_magick'
+
+module MiniMagick
+  class Image
+    def pixel_at(x, y)
+      run_command("convert", "#{path}[1x1+#{x.to_i}+#{y.to_i}]", 'txt:').split("\n").each do |line|
+        return $1 if /^0,0:.*(#[0-9a-fA-F]+)/.match(line)
+      end
+      nil
+    end
+  end
+end
+
+class ImageProcessor
+  IMAGE_SIZES = [640, 1024]
+
+  def initialize(post)
+    @post = post
+    @site = post.site
+  end
+
+  def process!
+    doc = Nokogiri::HTML.fragment(@post.output)
+    doc.css('img').each do |node|
+      next unless src = node['src']
+      next if src.start_with?('http')
+      next unless src.end_with?('png') || src.end_with?('jpg')
+
+      path = ".#{src}"
+      unless File.exist?(path)
+        puts "--------> Missing image #{path}"
+        next
+      end
+
+      process_image(node)
+    end
+
+    @post.output = doc.to_html
+  end
+
+  private
+
+  def process_image(node)
+    path = node['src']
+    full_path = ".#{path}"
+    srcset = []
+    original_width = MiniMagick::Image.open(full_path).width.to_i
+
+    IMAGE_SIZES.each do |size|
+      next unless size < original_width
+
+      resized_path = path.sub(/\.(png|jpg)$/, "-#{size}w.\\1")
+      full_resized_path = ".#{resized_path}"
+      srcset << "#{resized_path} #{size}w"
+      @site.static_files << Jekyll::StaticFile.new(@site, '.', File.dirname(full_resized_path), File.basename(full_resized_path))
+
+      next if File.exist?(full_resized_path)
+
+      image = MiniMagick::Image.open(full_path)
+      image.resize("#{size}x#{size}>")
+      image.write(full_resized_path)
+    end
+
+    unless srcset.blank?
+      srcset << "#{path} #{original_width}w"
+      node['srcset'] = srcset.join(', ')
+      node['sizes'] = '80vw'
+    end
+
+    if path.end_with?('jpg')
+      image = MiniMagick::Image.open(full_path)
+      image.resize('1x1')
+      if color = image.pixel_at(1, 1)
+        node['style'] = "background-color:#{color.downcase}"
+      end
+    end
+  end
+end
+
+Jekyll::Hooks.register :posts, :post_render do |post|
+  ImageProcessor.new(post).process!
+end
--- _plugins/rewrite_images.rb
+++ _plugins/rewrite_images.rb
@@ -1,4 +1,3 @@
-require 'nokogiri'
 require 'mini_magick'
 
 class Jekyll::Document
@@ -11,20 +10,7 @@ class Jekyll::Document
   end
 end
 
-module MiniMagick
-  class Image
-    def pixel_at(x, y)
-      run_command("convert", "#{path}[1x1+#{x.to_i}+#{y.to_i}]", 'txt:').split("\n").each do |line|
-        return $1 if /^0,0:.*(#[0-9a-fA-F]+)/.match(line)
-      end
-      nil
-    end
-  end
-end
-
 class RewriteImages < Jekyll::Generator
-  IMAGE_SIZES = [640, 1024]
-
   def generate(site)
     @site = site
     markdown_converter = @site.find_converter_instance(Jekyll::Converters::Markdown)
@@ -43,65 +29,8 @@ class RewriteImages < Jekyll::Generator
         document.data['cover_image_width'] = size[0]
         document.data['cover_image_height'] = size[1]
       end
-
-      doc = Nokogiri::HTML.fragment(document.content)
-      doc.css('img').each do |node|
-        next unless src = node['src']
-        next if src.start_with?('http')
-        next unless src.end_with?('png') || src.end_with?('jpg')
-
-        path = ".#{src}"
-        unless File.exist?(path)
-          puts "--------> Missing image #{path}"
-          next
-        end
-
-        # puts "        - Processing #{src}"
-        process_image(node)
-      end
-
-      document.content = doc.to_html
     end
 
     puts '        - Rewrite Images'
   end
-
-  private
-
-  def process_image(node)
-    path = node['src']
-    full_path = ".#{path}"
-    srcset = []
-    original_width = MiniMagick::Image.open(full_path).width.to_i
-
-    IMAGE_SIZES.each do |size|
-      next unless size < original_width
-
-      resized_path = path.sub(/\.(png|jpg)$/, "-#{size}w.\\1")
-      full_resized_path = ".#{resized_path}"
-      srcset << "#{resized_path} #{size}w"
-      @site.static_files << Jekyll::StaticFile.new(@site, '.', File.dirname(full_resized_path), File.basename(full_resized_path))
-
-      next if File.exist?(full_resized_path)
-
-      image = MiniMagick::Image.open(full_path)
-      image.resize("#{size}x#{size}>")
-      image.write(full_resized_path)
-    end
-
-    unless srcset.blank?
-      srcset << "#{path} #{original_width}w"
-      node['srcset'] = srcset.join(', ')
-      node['sizes'] = '80vw'
-    end
-
-    if path.end_with?('jpg')
-      image = MiniMagick::Image.open(full_path)
-      image.resize('1x1')
-      if color = image.pixel_at(1, 1)
-        node['style'] = "background-color:#{color.downcase}"
-      end
-    end
-  end
 end
-
